6c84c0cd951d1bb455b97ba49ae1e9315ee155bf
WATERWORTHITPC\nwate_000 <n.waterworth@verizon.net>
2018-11-23 12:01:21 -0600

    Started item implementation, have example of how to loop through list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavParams } from 'ionic-angular';
-import { PageService } from '../../providers/PageService';
+import { ExpirationDataServiceProvider } from '../../providers/expiration-data-service';
+var items = require("../../data/items.json");
 
 @Component({
   selector: 'page-home',
@@ -12,12 +13,20 @@ export class HomePage {
   Type = 0;
   sortBy = 'none';
   down = false;
+  Food = null;
 
-  constructor(public navParams: NavParams) {
+  constructor(public navParams: NavParams, public exp: ExpirationDataServiceProvider) {
     this.Type = navParams.get("Type");
+    this.Food = items.Food;
   }
 
   sorTog(s){
     this.sortBy = s;
+  }
+
+  getDate(name,date,type){
+
+    this.exp.getExpirationDate();
+    document.write("<p>" + Date() + "</p>");
   }
 }
