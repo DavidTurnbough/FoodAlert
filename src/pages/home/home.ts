caaf5d3bcc4c63800e1ac7ee76c0d044661521ce
nachoz12341 <nachoz12341@gmail.com>
2018-11-24 16:12:03 -0600

    Moved scanner into proper page
    Now shows result in toast text properly
    Page navigation works as expected

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { NavParams } from 'ionic-angular';
 import { PageService } from '../../providers/PageService';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-import { Toast } from '@ionic-native/toast'
 
 @Component({
   selector: 'page-home',
@@ -13,21 +11,9 @@ export class HomePage {
   //Page Variables
   Type = 0;
 
-  constructor(public navParams: NavParams, private barcodeScanner: BarcodeScanner, private toast: Toast) {
+  constructor(public navParams: NavParams) {
     this.Type = navParams.get("Type");
   }
 
-  scan()
-  {
-    this.barcodeScanner.scan({showTorchButton: true,showFlipCameraButton: true,resultDisplayDuration: 0}).then(barcodeData => {
 
-      this.toast.show(barcodeData.toString(),'2000','center').subscribe(
-        toast => {console.log(toast);}
-      );
-
-    console.log('Barcode data', barcodeData);
-    }).catch(err => {
-      console.log('Error', err);
-    });
-  }
 }
--- src/pages/picture/picture.ts
+++ src/pages/picture/picture.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams} from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Toast } from '@ionic-native/toast'
 
 @Component({
   selector: 'page-Picture',
@@ -8,7 +10,21 @@ import { NavController, NavParams} from 'ionic-angular';
 
 export class PicturePage{
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner, private toast: Toast) {
+  }
+
+  ionViewWillEnter()
+  {
+    this.barcodeScanner.scan({showTorchButton: true,showFlipCameraButton: true,resultDisplayDuration: 0}).then(barcodeData => {
+      this.toast.show(barcodeData.text,'2000','center').subscribe(
+        toast => {console.log(toast);}
+        );
+
+    this.navCtrl.pop(); //Goes back to the previous page
+
+    }).catch(err => {
+      console.log('Error', err);
+    });
   }
 
 }
