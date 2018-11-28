5ca094c101ca5b47f18000f30e09b42c50a4bbb7
nachoz12341 <nachoz12341@gmail.com>
2018-11-27 20:14:53 -0600

    Initial notification push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Nav } from 'ionic-angular';
 import { PageService } from '../providers/PageService';
 import { CalenderPage } from '../pages/calender/calender';
 import { ItemDataServiceProvider } from '../providers/item-data-service';
+import { ExpirationDataServiceProvider } from '../providers/expiration-data-service';
+import { NotifcationServiceProvider } from '../providers/notifcation-service';
 
 
 @Component({
@@ -16,13 +18,28 @@ export class MyApp {
   rootPage:any = CalenderPage;
   @ViewChild(Nav) navCtrl: Nav;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public pageService: PageService, public item: ItemDataServiceProvider) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public pageService: PageService, public expirationData: ExpirationDataServiceProvider,public item: ItemDataServiceProvider, public notification: NotifcationServiceProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    //Clear previous notifications then
+    this.notification.clearAllNotifications();
+    this.addAllNotifications();
+  }
+
+  addAllNotifications()
+  {
+    var list = this.item.getAllData();
+
+    for(var i=0; i<list.length;i++)
+    {
+      var notificationDate = this.expirationData.getExpirationDate(list[i].name,list[i].date,list[i].type);
+      this.notification.setNotification(i,list[i].name,notificationDate,notificationDate);
+    }
   }
 
   goCalender(){
